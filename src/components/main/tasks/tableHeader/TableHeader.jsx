47d457796a8d59aa680a3f0430ccf37d9cc4b572
Sasha Chubarov <sasha.chubarov.2022@gmail.com>
2023-11-11 14:03:14 +0200

    Finish making validation

--- src/components/main/tasks/tableHeader/TableHeader.jsx
+++ src/components/main/tasks/tableHeader/TableHeader.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from 'react';
 import m from '../../Main.module.css'
+import { validate } from './validate';
 
-const TableHeader = () => {
+const TableHeader = (props) => {
 
     let timeStart = useRef({
         hours: null,
@@ -13,68 +14,56 @@ const TableHeader = () => {
         minutes: null
     })
 
-    function validate() {
+    let taskName = useRef('');
 
-        if (!!timeStart.current.hours) {
-            if (timeStart.current.hours < 0 || timeStart.current.minutes < 0) {
-                console.log('В первой дате часы и минуты меньше 0');
-            } else if (timeStart.current.hours > 24 || timeStart.current.minutes > 59) {
-                console.log('В первой дате часы и минуты больше 24 или 59');
-            } else {
-                console.log('Первая дата корректна');
-            }
+    let onSubmite = () => {
+        let response = validate(timeStart, timeEnd, props.addZero);
+        if (isNaN(response)) {
+            props.addTask(response, taskName.current);
         }
-        if (!!timeEnd.current.hours && !!timeStart.current.hours) {
-            if (timeEnd.current.hours > 24 || timeEnd.current.minutes > 59) {
-                console.log('Во второй дате часы и минуты больше 24 или 59!');
-            } else if (timeEnd.current.hours < 0 || timeEnd.current.minutes < 0) {
-                console.log('Во второй дате часы и минуты меньше 0!');
-            } else if (timeStart.current.hours < timeEnd.current.hours) {
-                console.log('Первая дата больше второй!');
-            } else if (timeStart.current.hours == timeEnd.current.hours && timeStart.current.minutes >= timeEnd.current.minutes) {
-                console.log('Часы равны, но минуты неправильны!');
-            } else {
-                console.log('Вторая дата корректна!');
-            }
-        }
-
-        /* попробовать 12 12 - 12 12
-        12 12   11 12   
-        12 12      12
-           12   11 12
-        11      12 12
-           12   10 12
-        и по всякому*/
-
     }
-
-    function resetTime() {
-        timeStart.current.hours = null;
-        timeStart.current.minutes = null;
-        timeEnd.current.hours = null;
-        timeEnd.current.minutes = null;
-    }
-
+    
     return (
         <div className={m.first__line}>
             <div className={m.first__column}>
                 <div className={m.left__part}>
-                    <input className={m.time__inputs} onChange={(e) => timeStart.current.hours = e.target.value} type="number" placeholder='12' maxLength={2} />
+                    <input className={m.time__inputs} 
+                            onChange={(e) => timeStart.current.hours = +e.target.value} 
+                            type="number" 
+                            placeholder='12' 
+                            maxLength={2} />
                     :
-                    <input className={m.time__inputs} onChange={(e) => timeStart.current.minutes = e.target.value} type="number" placeholder='00' maxLength={2} />
+                    <input className={m.time__inputs} 
+                            onChange={(e) => timeStart.current.minutes = +e.target.value} 
+                            type="number" 
+                            placeholder='00' 
+                            maxLength={2} />
                 </div>
                 -
                 <div className={m.right__part}>
-                    <input className={m.time__inputs} onChange={(e) => timeEnd.current.hours = e.target.value} type="number" placeholder='18' maxLength={2} />
+                    <input  className={m.time__inputs} 
+                            onChange={(e) => timeEnd.current.hours = +e.target.value} 
+                            type="number" placeholder='18' 
+                            maxLength={2} />
                     :
-                    <input className={m.time__inputs} onChange={(e) => timeEnd.current.minutes = e.target.value} type="number" placeholder='00' maxLength={2} />
+                    <input  className={m.time__inputs} 
+                            onChange={(e) => timeEnd.current.minutes = +e.target.value} 
+                            type="number" 
+                            placeholder='00' 
+                            maxLength={2} />
                 </div>
             </div>
             <div className={m.second__column}>
-                <textarea className={m.name__inputs} name="task-name" maxLength={80} cols="30" rows="1" placeholder='Enter a task name...'></textarea>
+                <textarea className={m.name__inputs}
+                          onChange={(e) => taskName.current = e.target.value}
+                          name="task-name" 
+                          maxLength={80} 
+                          cols="30" 
+                          rows="1" 
+                          placeholder='Enter a task name...'></textarea>
             </div>
             <div className={m.third__column}>
-                <button onClick={() => validate()}>
+                <button onClick={onSubmite}>
                     <img src="add.png" className={m.add_icon} alt="true" />
                 </button>
             </div>
---
+++ src/components/main/tasks/tableHeader/validate.js
@@ -0,0 +1,51 @@
+export function validate(timeStart, timeEnd, addZero) {
+    let error = 0;
+
+    //validate time from
+    if (!!timeStart.current.hours) {
+        if (timeStart.current.hours < 0 || timeStart.current.minutes < 0) {
+            alert('В первой дате часы и минуты меньше 0');
+            error++
+        } else if (timeStart.current.hours >= 24 || timeStart.current.minutes > 59) {
+            alert('В первой дате часы и минуты больше 24 или 59');
+            error++
+        }
+    } else if (!!timeStart.current.minutes) {
+        alert('Укажите точное время первой даты');
+        error++
+    }
+
+    //validate time to
+    if (!!timeEnd.current.hours && !!timeStart.current.hours) {
+        if (timeEnd.current.hours >= 24 || timeEnd.current.minutes > 59) {
+            alert('Во второй дате часы и минуты больше 24 или 59!');
+            error++
+        } else if (timeEnd.current.hours < 0 || timeEnd.current.minutes < 0) {
+            alert('Во второй дате часы и минуты меньше 0!');
+            error++
+        } else if (timeStart.current.hours > timeEnd.current.hours) {
+            alert('Первая дата больше второй!');
+            error++
+        } else if (timeStart.current.hours === timeEnd.current.hours && timeStart.current.minutes >= timeEnd.current.minutes) {
+            alert('Часы равны, но минуты неправильны!');
+            error++
+        }
+    } else if (!timeStart.current.hours && !!timeEnd.current.hours) {
+        alert('Укажите точное время первой даты!');
+        error++
+    } else if (!!timeEnd.current.minutes) {
+        alert('Укажите точное время второй даты!');
+        error++
+    }
+
+    if(error === 0) {
+        return {
+            tSH: addZero(timeStart.current.hours), //tSH - time start hours
+            tSM: addZero(timeStart.current.minutes), // tSH - time start minutes
+            tEH: addZero(timeEnd.current.hours),
+            tEM: addZero(timeEnd.current.minutes)
+        }
+    } else {
+        return error;
+    }
+}
